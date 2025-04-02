5ead3763bc8dc63b6ee4c595fc3c86efdbbc18ae
Funkeymantic <martincpaul2000@gmail.com>
2025-04-01 23:56:11 -0700

    Fix Twitch notifier

--- cogs/twitchNotifier.py
+++ cogs/twitchNotifier.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands, tasks
 import aiohttp
 import os
+import asyncio
 
-class LiveNotifier(commands.Cog):
+class TwitchNotifier(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.twitch_client_id = os.getenv("TWITCH_CLIENT_ID")
@@ -14,11 +15,13 @@ class LiveNotifier(commands.Cog):
         self.token = None
         self.streamers = {}
         self.live_messages = {}
-        self.check_streamers.start()
 
     async def cog_load(self):
+        await self.bot.wait_until_ready()
+        await self.get_twitch_token()
         await self.refresh_twitch_users()
-        print("[LiveNotifier] Cog loaded and stream check started.")
+        self.check_streamers.start()
+        print("[TwitchNotifier] Cog loaded and stream check started.")
 
     async def get_twitch_token(self):
         url = "https://id.twitch.tv/oauth2/token"
@@ -34,7 +37,11 @@ class LiveNotifier(commands.Cog):
                     self.token = data["access_token"]
 
     async def refresh_twitch_users(self):
-        guild = self.bot.guilds[0]  # Assuming single guild
+        await self.bot.wait_until_ready()
+        if not self.bot.guilds:
+            print("[TwitchNotifier] No guilds connected yet.")
+            return
+        guild = self.bot.guilds[0]  # Assumes single guild
         role = discord.utils.get(guild.roles, name=self.performance_role_name)
         if role:
             self.streamers = {member.display_name: member for member in role.members}
@@ -65,15 +72,15 @@ class LiveNotifier(commands.Cog):
 
             if is_live:
                 if streamer_name not in self.live_messages and channel:
-                    message = await channel.send(f"{streamer_name} is now live! https://twitch.tv/{streamer_name}")
-                    self.live_messages[streamer_name] = message.id
+                    msg = await channel.send(f"{streamer_name} is now live! https://twitch.tv/{streamer_name}")
+                    self.live_messages[streamer_name] = msg.id
             else:
                 if streamer_name in self.live_messages and channel:
                     try:
-                        message = await channel.fetch_message(self.live_messages.pop(streamer_name))
-                        await message.delete()
+                        msg = await channel.fetch_message(self.live_messages.pop(streamer_name))
+                        await msg.delete()
                     except discord.NotFound:
                         pass
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(LiveNotifier(bot))
+    await bot.add_cog(TwitchNotifier(bot))
